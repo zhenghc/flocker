ec2e6e96e9ad0944a84d007dc6e8dc8e29aa9760
Richard Wall <richard.wall@clusterhq.com>
2014-09-12 10:04:36 +0100

    A skeleton for driving some wordish tests.

---
+++ flocker/functional/test_tutorial.py
@@ -0,0 +1,16 @@
+# Copyright Hybrid Logic Ltd.  See LICENSE file for details.
+
+"""
+Functional tests for the ``flocker-volume`` command line tool.
+"""
+
+from twisted.trial.unittest import TestCase
+
+
+class MovingApplicationsTests(TestCase):
+    """
+    """
+    def test_all(self):
+        """
+        """
+        self.fail()
