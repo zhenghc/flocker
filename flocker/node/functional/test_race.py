82aa19f74bbb8180415649bd0f5a7620d674423b
Richard Wall <richard.wall@clusterhq.com>
2014-11-27 11:30:09 +0000

    Add some docs

--- flocker/node/functional/test_race.py
+++ flocker/node/functional/test_race.py
@@ -1,3 +1,40 @@
+# Copyright Hybrid Logic Ltd.  See LICENSE file for details.
+
+"""
+Tests to reproduce Docker race conditions.
+
+Requirements:
+ * https://github.com/docker/docker-py
+
+Tests may only fail when repeated.
+
+For example, using `trial`:
+
+  $ trial --until-failure test_race.py
+  ...
+
+  Test Pass 7
+  flocker.node.functional.test_race
+    DockerClientTests
+      test_add_and_remove ...                                             [ERROR]
+
+  ===============================================================================
+  [ERROR]
+  Traceback (most recent call last):
+    File "/usr/lib64/python2.7/unittest/case.py", line 369, in run
+      testMethod()
+    File "/vagrant/flocker/node/functional/test_race.py", line 25, in test_add_and_remove
+      client.stop(container=name)
+    File "/home/vagrant/.virtualenvs/940/lib/python2.7/site-packages/docker/client.py", line 904, in stop
+      self._raise_for_status(res)
+    File "/home/vagrant/.virtualenvs/940/lib/python2.7/site-packages/docker/client.py", line 88, in _raise_for_status
+      raise errors.APIError(e, response, explanation=explanation)
+  docker.errors.APIError: 500 Server Error: Internal Server Error ("Cannot stop container 464635515479: no such process")
+
+  flocker.node.functional.test_race.DockerClientTests.test_add_and_remove
+  -------------------------------------------------------------------------------
+"""
+
 from random import random
 from unittest import TestCase
 from docker import Client
