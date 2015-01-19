59cd7ed1b92196ccbb79f6e11adc63883d7616f5
Richard Wall <richard.wall@clusterhq.com>
2015-01-19 14:47:16 +0000

    Import some experimental vagrant scripts

---
+++ vagrant/install-devstack.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Checkout devstack
+sudo apt-get install git
+git clone https://git.openstack.org/openstack-dev/devstack
+cd devstack
+
+# Switch to stable Juno branch:
+git checkout stable/juno
+
+# Create config file with default passwords:
+echo '[[local|localrc]]' > local.conf
+echo ADMIN_PASSWORD=password >> local.conf
+echo MYSQL_PASSWORD=password >> local.conf
+echo RABBIT_PASSWORD=password >> local.conf
+echo SERVICE_PASSWORD=password >> local.conf
+echo SERVICE_TOKEN=tokentoken >> local.conf
+echo OFFLINE=True >> local.conf
+
+# Start OpenStack:
+./stack.sh
